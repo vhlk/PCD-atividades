7305f443dc5611d551c64e70ea7e405f557c127e
viniciussousav <vsv@cin.ufpe.br>
2021-09-28 21:07:08 -0300

    Ajeitando Sync.cs

--- atividade-1/CSharp/Sync.cs
+++ atividade-1/CSharp/Sync.cs
@@ -2,65 +2,62 @@ using System;
 using System.Collections.Generic;
 using System.Threading;
 
-namespace Sync
+class Bridge : SemaphoreSlim
 {
-    class Bridge : SemaphoreSlim
+    public readonly Dictionary<char, int> FirstQueueCar;
+    public Bridge() : base(1, 1)
     {
-        public readonly Dictionary<char, int> FirstQueueCar;
-        public Bridge() : base(1, 1)
-        {
-            FirstQueueCar = new Dictionary<char, int>() {{'L', 0}, {'R', 0}};
-        }
+        FirstQueueCar = new Dictionary<char, int>() {{'L', 0}, {'R', 0}};
     }
-    
-    class Program
+}
+
+class Program
+{
+    static void Main()
     {
-        static void Main()
+        Console.WriteLine("Enter the number of car on the right side of the bridge");
+        var numberCarsRightSide = int.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
+        Console.WriteLine("Enter the number of car on the left side of the bridge");
+        var numberCarsLeftSide = int.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
+        
+        var bridge = new Bridge();
+
+        List<Thread> allThreads = new List<Thread>();
+        
+        for (var i = 0; i < numberCarsRightSide; i++)
         {
-            Console.WriteLine("Enter the number of car on the right side of the bridge");
-            var numberCarsRightSide = int.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
-            Console.WriteLine("Enter the number of car on the left side of the bridge");
-            var numberCarsLeftSide = int.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
-            
-            var bridge = new Bridge();
-
-            List<Thread> allThreads = new List<Thread>();
-            
-            for (var i = 0; i < numberCarsRightSide; i++)
-            {
-                var position = i;
-                var carThread = new Thread(() => CrossBridge(bridge, position, 'R'));
-                allThreads.Add(carThread);
-            }
-            
-            for (var i = 0; i < numberCarsLeftSide; i++)
-            {
-                var position = i;
-                var carThread = new Thread(() => CrossBridge(bridge, position, 'L'));
-                allThreads.Add(carThread);
-            }
-
-            allThreads.ForEach(thread => thread.Start());
-            
-            Console.ReadLine();
+            var position = i;
+            var carThread = new Thread(() => CrossBridge(bridge, position, 'R'));
+            allThreads.Add(carThread);
         }
         
-        private static void CrossBridge(Bridge bridge, int position, char side)
+        for (var i = 0; i < numberCarsLeftSide; i++)
         {
-            Console.WriteLine("ARRIVE: Car {0} is waiting your time at {1} side", position, side);
-            while (bridge.FirstQueueCar[side] <= position)
-            {
-                if (bridge.FirstQueueCar[side] != position) 
-                    continue;
-                
-                Console.WriteLine("READY: Car {0} is ready to cross the bridge at {1} side", position, side);
+            var position = i;
+            var carThread = new Thread(() => CrossBridge(bridge, position, 'L'));
+            allThreads.Add(carThread);
+        }
 
-                bridge.Wait();
-                Console.WriteLine("CROSSING: Car {0} is crossing the bridge starting from {1} side", position, side);
-                bridge.FirstQueueCar[side] += 1;
-                Console.WriteLine("FINISHED: Car {0} finished to cross the bridge starting from {1} side", position, side);
-                bridge.Release();
-            }
+        allThreads.ForEach(thread => thread.Start());
+        
+        Console.ReadLine();
+    }
+    
+    private static void CrossBridge(Bridge bridge, int position, char side)
+    {
+        Console.WriteLine("ARRIVE: Car {0} is waiting your time at {1} side", position, side);
+        while (bridge.FirstQueueCar[side] <= position)
+        {
+            if (bridge.FirstQueueCar[side] != position) 
+                continue;
+            
+            Console.WriteLine("READY: Car {0} is ready to cross the bridge at {1} side", position, side);
+
+            bridge.Wait();
+            Console.WriteLine("CROSSING: Car {0} is crossing the bridge starting from {1} side", position, side);
+            bridge.FirstQueueCar[side] += 1;
+            Console.WriteLine("FINISHED: Car {0} finished to cross the bridge starting from {1} side", position, side);
+            bridge.Release();
         }
     }
 }

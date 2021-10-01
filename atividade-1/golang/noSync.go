4d279dec2d0ad09bb51476df845e8f56db61c0b4
Victor Hugo <vhlk@cin.ufpe.br>
2021-10-01 10:15:36 -0300

    refactor of no sync code

--- atividade-1/golang/noSync.go
+++ atividade-1/golang/noSync.go
@@ -1,44 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
-func leftNoSync(carNumber int, hasCar *bool) {
-	fmt.Printf("ARRIVE: Car %d is waiting your time at L side\n", carNumber)
-	fmt.Printf("CROSSING: Car %d is crossing the bridge starting from L side\n", carNumber)
+func crossBridgeNoSync(carNumber int, hasCar *bool, sideLetter byte) {
+	fmt.Printf("ARRIVE: Car %d is waiting your time at %c side\n", carNumber, sideLetter)
+	fmt.Printf("CROSSING: Car %d is crossing the bridge starting from %c side\n", carNumber, sideLetter)
 	if *hasCar { fmt.Printf("**************** COLISION DETECTED (%d) ****************\n", carNumber) }
 	*hasCar = true
-	fmt.Printf("FINISHED: Car %d finished to cross the bridge starting from L side\n", carNumber)
-	*hasCar = false
-}
-
-func rightNoSync(carNumber int, hasCar *bool) {
-	fmt.Printf("ARRIVE: Car %d is waiting your time at R side\n", carNumber)
-	fmt.Printf("CROSSING: Car %d is crossing the bridge starting from R side\n", carNumber)
-	if *hasCar { fmt.Printf("**************** COLISION DETECTED (%d) ****************\n", carNumber) }
-	*hasCar = true
-	fmt.Printf("FINISHED: Car %d finished to cross the bridge starting from R side\n", carNumber)
+	fmt.Printf("FINISHED: Car %d finished to cross the bridge starting from %c side\n", carNumber, sideLetter)
 	*hasCar = false
 }
 
 func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+
 	println("Enter the number of car on the right side of the bridge:")
-	var carrosDireita int
-	_, _ = fmt.Scan(&carrosDireita)
+	scanner.Scan()
+	carrosDireita, _ := strconv.Atoi(scanner.Text())
 
 	println("Enter the number of car on the left side of the bridge:")
-	var carrosEsquerda int
-	_, _ = fmt.Scan(&carrosEsquerda)
+	scanner.Scan()
+	carrosEsquerda, _ := strconv.Atoi(scanner.Text())
 
 	var hasCar bool
 	hasCar = false
 
-	for i := 0; i < carrosDireita; i++ {
-		go rightNoSync(i, &hasCar)
-	}
+	for i := 0; i < carrosDireita; i++ { go crossBridgeNoSync(i, &hasCar, 'R')	}
 
-	for i := 0; i < carrosEsquerda; i++ {
-		go leftNoSync(i, &hasCar)
-	}
+	for i := 0; i < carrosEsquerda; i++ { go crossBridgeNoSync(i, &hasCar, 'L') }
 
 	_,_ = fmt.Scanln()
 }

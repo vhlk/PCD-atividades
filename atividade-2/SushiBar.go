b9b4cd988cc66026e20d6e6257caf3105adc0220
Victor Hugo <vhlk@cin.ufpe.br>
2021-10-18 09:35:50 -0300

    show ids

--- atividade-2/SushiBar.go
+++ atividade-2/SushiBar.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 	"strconv"
+	"time"
 )
 
 type ISushiBar interface {
@@ -13,28 +15,30 @@ type ISushiBar interface {
 
 type SushiBar chan struct {}
 
-func NewSushiBar(n int32) *SushiBar {
+func NewSushiBar(n int) *SushiBar {
 	sb := SushiBar(make(chan struct{}, n))
 
 	return &sb
 }
 
-func (sb *SushiBar) saiu() {
+func (sb *SushiBar) saiu(id int) {
+	time.Sleep(10)
 	<- *sb
-	println("Saiu 1 cara")
+	fmt.Printf("O %d saiu\n", id)
 }
 
-func (sb *SushiBar) chegou() {
-	println("Chegou 1 cara e ta esperando")
+func (sb *SushiBar) chegou(id int) {
+	fmt.Printf("Chegou o %d e ta esperando\n", id)
 	*sb <- struct {}{}
-	println("Chegou 1 cara e entrou")
-	sb.saiu()
+	fmt.Printf("O %d entrou\n", id)
+	sb.saiu(id)
 }
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	sb := NewSushiBar(5)
 
+	id := 0
 	for true {
 		println("Enter the number of people to arrive or press q to quit:")
 		scanner.Scan()
@@ -44,7 +48,8 @@ func main() {
 		pessoas, _ := strconv.Atoi(usrText)
 
 		for i := 0; i < pessoas; i++ {
-			go sb.chegou()
+			go sb.chegou(id)
+			id ++
 		}
 	}
 }

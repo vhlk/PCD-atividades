9bf7b68bd9f5121e8f73952cbd2cbd3bf23527ef
Victor Hugo <vhlk@cin.ufpe.br>
2021-11-11 21:56:03 -0300

    inicio da atividade 5 - Java

---
+++ atividade-5/Java/SocketClienteServidor/src/tcp/Cliente.java
@@ -0,0 +1,52 @@
+package tcp;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Cliente {
+	private static final int SERVER_PORT = 8888;
+
+	public static void main(String[] args) {
+		Socket socket = null; 
+		try {
+			socket = new Socket("localhost", SERVER_PORT);
+		} catch (Exception e) {
+			System.err.println("Erro ao se conectar: "+e.getMessage());
+			return;
+		}
+		
+		PrintWriter pw = null;
+		try {
+			pw = new PrintWriter(socket.getOutputStream());
+			pw.println("abcdef");
+			pw.flush();
+			
+		} catch (IOException e) {	
+			System.err.println("Erro ao enviar a mensagem!");
+			return;
+		}
+		
+		InputStream is = null;
+		BufferedReader br = null;
+		try {
+			is = socket.getInputStream();
+			br = new BufferedReader(new InputStreamReader(is));
+			System.out.println(br.readLine());
+		} catch (IOException e) { 
+			System.err.println("Erro ao receber mensagem!");
+			return;
+		}
+		
+		try {
+			socket.close();
+		} catch (IOException ignored) { }
+		
+	}
+
+}
---
+++ atividade-5/Java/SocketClienteServidor/src/tcp/Server.java
@@ -0,0 +1,86 @@
+package tcp;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+	private static final int SERVER_PORT = 8888; 
+	private static boolean stopServer = false;
+	protected static final PrintStream cOut = System.err;
+
+	public static void main(String[] args) {
+		ServerSocket sSocket = null;
+		try {
+			sSocket = new ServerSocket(SERVER_PORT);
+		} catch (IOException e) { 
+			cOut.println("Não foi possível iniciar o servidor :(");	
+			System.exit(-1);
+		}
+	
+		while (true && !stopServer) {
+			Socket cSocket = null;
+			try {
+				cSocket = sSocket.accept();
+			} catch (IOException e) { cOut.println("Erro ao aceitar cliente :( " + e.getMessage()); }
+			
+			new AcceptClientThread(cSocket).start();
+		}
+		
+		try {
+			sSocket.close();
+		} catch (IOException ignored) { }
+	}
+
+}
+
+class AcceptClientThread extends Thread {
+	private Socket socket;
+	
+	public AcceptClientThread(Socket sck) {
+		this.socket = sck;
+	}
+	
+	public void run() {
+		InputStream is = null;
+		BufferedReader br = null;
+		PrintWriter pw = null;
+		try {
+			is = socket.getInputStream();
+			br = new BufferedReader(new InputStreamReader(is));
+			pw = new PrintWriter(socket.getOutputStream());			
+		} catch (IOException e) {
+			System.err.println("Não foi possível pegar as informações sobre o cliente :( "+e.getMessage());
+			return;
+		}
+		
+		String data = "";
+		
+		while (true) {
+			try {
+				data = br.readLine();
+				
+				if (data == null || data == "") {
+					pw.println("Erro!");
+					continue;
+				}
+				
+				System.out.println("Recebido: "+data);
+				
+				if (data.toLowerCase() == "q") return;
+				
+				pw.println("Você digitou: "+data);
+				pw.flush();
+			} catch (IOException e) {
+				System.err.println("Erro na transmissão dos dados :(");
+				return;
+			}
+		}
+	}
+}
